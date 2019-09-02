89b0a8783255f0e433292b741feb4c9a5e90a0fa
Andreas Stromberg <andreas.stromberg90@gmail.com>
2019-09-02 02:06:44 +0200

    Working preselection on hover

--- Assets/Editor/StrombergTools.cs
+++ Assets/Editor/StrombergTools.cs
@@ -63,6 +63,8 @@ namespace StrombergTools {
     [InitializeOnLoad]
     public class Commands : Editor {
         static StrombergToolsSettings Settings;
+        private static GameObject GameObjectUnderMouse;
+
         static Commands() {
             var settings = new StrombergToolsSettings();
             settings.shortcuts[1].command = Command.ToggleHide;
@@ -74,6 +76,15 @@ namespace StrombergTools {
 
         static void OnDestroy() {
             SceneView.duringSceneGui -= OnSceneGUI;
+        }
+
+        static Color polygon_color = new Color(0.7f, 0.2f, 0f, 0.3f);
+        static Color edge_color = new Color(1f, 0f, 0f, 0.8f);
+
+        struct Triangle {
+            public Vector3 v1;
+            public Vector3 v2;
+            public Vector3 v3;
         }
 
         static void OnSceneGUI(SceneView sceneView) {
@@ -92,16 +103,51 @@ namespace StrombergTools {
                     }
                 }
             }
-            if(Utils.InsideSceneView(sceneView, Event.current.mousePosition)) {
-                var ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-                var obj = HandleUtility.RaySnap(ray);
+            if (Event.current.type == EventType.MouseMove) {
+                var obj = HandleUtility.PickGameObject(Event.current.mousePosition, true);
                 if (obj != null) {
-                    var hit = (RaycastHit)obj;
-                    if(hit.collider.gameObject != Selection.activeGameObject) {
-                        Handles.color = Color.yellow;
-                        Handles.DrawWireCube(hit.collider.transform.position, hit.collider.bounds.size);
-                        sceneView.Repaint();
+                    if (obj.gameObject != Selection.activeGameObject && obj.gameObject.GetComponentInChildren<MeshRenderer>()) {
+                        GameObjectUnderMouse = obj;
                     }
+                    else {
+                        GameObjectUnderMouse = null;
+                    }
+                }
+                else {
+                    GameObjectUnderMouse = null;
+                }
+            }
+            if (Utils.InsideSceneView(sceneView, Event.current.mousePosition)) {
+                if (GameObjectUnderMouse != null) {
+                    foreach (var mesh_filter in GameObjectUnderMouse.GetComponentsInChildren<MeshFilter>()) {
+                        var matrix = mesh_filter.gameObject.GetComponent<MeshRenderer>().localToWorldMatrix;
+                        for (var submesh_index = 0; submesh_index < mesh_filter.sharedMesh.subMeshCount; submesh_index++) {
+                            var triangles = mesh_filter.sharedMesh.GetTriangles(submesh_index);
+                            var current_triangle = new Triangle();
+                            var triangle_vertex = 0;
+                            var vertices = mesh_filter.sharedMesh.vertices;
+                            foreach (var triangle in triangles) {
+                                if (triangle_vertex == 0) {
+                                    current_triangle.v1 = vertices[triangle];
+                                }
+                                else if (triangle_vertex == 1) {
+                                    current_triangle.v2 = vertices[triangle];
+                                }
+                                else {
+                                    current_triangle.v3 = vertices[triangle];
+                                    var vertex_1 = matrix.MultiplyPoint(current_triangle.v1);
+                                    var vertex_2 = matrix.MultiplyPoint(current_triangle.v2);
+                                    var vertex_3 = matrix.MultiplyPoint(current_triangle.v3);
+                                    Handles.color = polygon_color;
+                                    Handles.DrawAAConvexPolygon(vertex_1, vertex_2, vertex_3);
+                                    Handles.color = edge_color;
+                                    Handles.DrawAAPolyLine(vertex_1, vertex_2, vertex_3);
+                                }
+                                triangle_vertex = (triangle_vertex + 1) % 3;
+                            }
+                        }
+                    }
+                    sceneView.Repaint();
                 }
             }
         }
